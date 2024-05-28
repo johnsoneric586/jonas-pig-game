f7e104fd86a39a7b5ab84de846a89e4d6abea0db
Eric Johnson <johnsoneric586@gmail.com>
2024-05-28 06:29:07 -0400

    Added the ability to 'hold' your score and add the current score to the total score using the hold button. Also modularized the 'switchPlayer' functionality to align with DRY.

--- script.js
+++ script.js
@@ -19,8 +19,17 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+
+function switchPlayer() {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+}
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -37,10 +46,18 @@ btnRoll.addEventListener('click', function () {
       currentScore;
   } else {
     // Switch player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener('click', function () {
+  // 1. Add current score to active player's score
+  scores[activePlayer] += currentScore;
+
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  // 2. Check if score is >= 100
+  // Finish game
+  // 3. If not, switch player
+  switchPlayer();
 });
